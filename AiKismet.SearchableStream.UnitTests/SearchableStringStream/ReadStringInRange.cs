70d3885b8925987b8255c3b3767d23a0ab06a99c
Dominik Gorecki <dom@nikgo.com>
2019-07-02 14:31:45 -0400

    Scaffold of unit tests to do

---
+++ AiKismet.SearchableStream.UnitTests/SearchableStringStream/ReadStringInRange.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchableStringStreamTests
+{
+    [TestClass]
+    public class ReadStringInRange
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void StartPositionBiggerThanEnd()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void StartPositionSameAsEnd()
+        {
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void StartPositionNegative()
+        {
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void EndPositionLargerThanStream()
+        {
+
+        }
+
+        [TestMethod]
+        public void EmptyStream()
+        {
+            var emptyByteArray = new byte[0];
+        }
+
+        [TestMethod]
+        public void ReadWholeStream()
+        {
+
+        }
+
+        [TestMethod]
+        public void ReadAtBeginning()
+        {
+
+        }
+
+        [TestMethod]
+        public void ReadAtEnd()
+        {
+
+        }
+
+        [TestMethod]
+        public void ReadInMiddle()
+        {
+
+        }
+
+    }
+}
